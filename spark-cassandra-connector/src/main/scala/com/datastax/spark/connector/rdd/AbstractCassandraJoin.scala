fafa6271be6b1427bbe3991cd1afaea5e4000edc
Alexey Ponkin <aponkin@luxoft.com>
2016-07-22 17:08:29 +0300

    Merge complete

--- spark-cassandra-connector/src/main/scala/com/datastax/spark/connector/rdd/AbstractCassandraJoin.scala
+++ spark-cassandra-connector/src/main/scala/com/datastax/spark/connector/rdd/AbstractCassandraJoin.scala
@@ -4,30 +4,47 @@ import org.apache.spark.metrics.InputMetricsUpdater
 
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.writer._
-import com.datastax.spark.connector.util.CqlWhereParser.{EqPredicate, InListPredicate, InPredicate, RangePredicate}
-import com.datastax.spark.connector.util.{CountingIterator, CqlWhereParser}
+import com.datastax.spark.connector.rdd.reader._
+import com.datastax.spark.connector.util.CqlWhereParser.{ EqPredicate, InListPredicate, InPredicate, RangePredicate }
+import com.datastax.spark.connector.util.{ CountingIterator, CqlWhereParser }
 import com.datastax.spark.connector.util.Quote._
 
 import com.datastax.driver.core.Session
 
-import org.apache.spark.{Partition, TaskContext}
+import org.apache.spark.{ Partition, TaskContext }
 import org.apache.spark.rdd.RDD
 
-private[rdd] trait AbstractCassandraJoin[L, O]{
+/**
+ * This trait contains shared methods from [[com.datastax.spark.connector.rdd.CassandraJoinRDD]] and
+ * [[com.datastax.spark.connector.rdd.CassandraLeftJoinRDD]] to avoid code duplication.
+ *
+ * @tparam L item type on the left side of the join (any RDD)
+ * @tparam R item type on the right side of the join (fetched from Cassandra)
+ */
+private[rdd] trait AbstractCassandraJoin[L, R] {
 
-  self: CassandraRDD[(L, O)] with (CassandraTableRowReaderProvider[R] forSome {type R}) =>
+  self: CassandraRDD[(L, R)] with CassandraTableRowReaderProvider[_] =>
 
   val left: RDD[L]
   val joinColumns: ColumnSelector
-
+  val manualRowWriter: Option[RowWriter[L]]
   implicit val rowWriterFactory: RowWriterFactory[L]
 
-  lazy val rowWriter = implicitly[RowWriterFactory[L]].rowWriter(
-    tableDef, joinColumnNames.toIndexedSeq)
+  private[rdd] def fetchIterator(
+    session: Session,
+    bsb: BoundStatementBuilder[L],
+    lastIt: Iterator[L]
+  ): Iterator[(L, R)]
+
+  lazy val rowWriter = manualRowWriter match {
+    case Some(_rowWriter) => _rowWriter
+    case None => implicitly[RowWriterFactory[L]].rowWriter(tableDef, joinColumnNames.toIndexedSeq)
+  }
 
   lazy val joinColumnNames: Seq[ColumnRef] = joinColumns match {
     case AllColumns => throw new IllegalArgumentException(
-      "Unable to join against all columns in a Cassandra Table. Only primary key columns allowed.")
+      "Unable to join against all columns in a Cassandra Table. Only primary key columns allowed."
+    )
     case PartitionKeyColumns =>
       tableDef.partitionKey.map(col => col.columnName: ColumnRef)
     case SomeColumns(cs @ _*) =>
@@ -35,12 +52,15 @@ private[rdd] trait AbstractCassandraJoin[L, O]{
       cs.map {
         case c: ColumnRef => c
         case _ => throw new IllegalArgumentException(
-          "Unable to join against unnamed columns. No CQL Functions allowed.")
+          "Unable to join against unnamed columns. No CQL Functions allowed."
+        )
       }
   }
 
-  /** This method will create the RowWriter required before the RDD is serialized.
-    * This is called during getPartitions */
+  /**
+   * This method will create the RowWriter required before the RDD is serialized.
+   * This is called during getPartitions
+   */
   protected def checkValidJoin(): Seq[ColumnRef] = {
     val partitionKeyColumnNames = tableDef.partitionKey.map(_.columnName).toSet
     val primaryKeyColumnNames = tableDef.primaryKey.map(_.columnName).toSet
@@ -53,7 +73,8 @@ private[rdd] trait AbstractCassandraJoin[L, O]{
     def checkSingleColumn(column: ColumnRef): Unit = {
       require(
         primaryKeyColumnNames.contains(column.columnName),
-        s"Can't pushdown join on column $column because it is not part of the PRIMARY KEY")
+        s"Can't pushdown join on column $column because it is not part of the PRIMARY KEY"
+      )
     }
 
     // Make sure we have all of the clustering indexes between the 0th position and the max requested
@@ -66,12 +87,14 @@ private[rdd] trait AbstractCassandraJoin[L, O]{
       val requiredColumns = tableDef.clusteringColumns.takeWhile(_.componentIndex.get <= maxIndex)
       val missingColumns = requiredColumns.toSet -- chosenClusteringColumns.toSet
       throw new IllegalArgumentException(
-        s"Can't pushdown join on column $maxCol without also specifying [ $missingColumns ]")
+        s"Can't pushdown join on column $maxCol without also specifying [ $missingColumns ]"
+      )
     }
     val missingPartitionKeys = partitionKeyColumnNames -- colNames
     require(
       missingPartitionKeys.isEmpty,
-      s"Can't join without the full partition key. Missing: [ $missingPartitionKeys ]")
+      s"Can't join without the full partition key. Missing: [ $missingPartitionKeys ]"
+    )
 
     joinColumnNames.foreach(checkSingleColumn)
     joinColumnNames
@@ -92,8 +115,8 @@ private[rdd] trait AbstractCassandraJoin[L, O]{
     require(
       joinColumnPredicates.isEmpty,
       s"""Columns specified in both the join on clause and the where clause.
-         |Partition key columns are always part of the join clause.
-         |Columns in both: ${joinColumnPredicates.mkString(", ")}""".stripMargin
+          |Partition key columns are always part of the join clause.
+          |Columns in both: ${joinColumnPredicates.mkString(", ")}""".stripMargin
     )
 
     logDebug("Generating Single Key Query Prepared Statement String")
@@ -113,16 +136,20 @@ private[rdd] trait AbstractCassandraJoin[L, O]{
     query
   }
 
+  private def boundStatementBuilder(session: Session): BoundStatementBuilder[L] = {
+    val protocolVersion = session.getCluster.getConfiguration.getProtocolOptions.getProtocolVersion
+    val stmt = session.prepare(singleKeyCqlQuery).setConsistencyLevel(consistencyLevel)
+    new BoundStatementBuilder[L](rowWriter, stmt, where.values, protocolVersion = protocolVersion)
+  }
+
   /**
    * When computing a CassandraPartitionKeyRDD the data is selected via single CQL statements
    * from the specified C* Keyspace and Table. This will be preformed on whatever data is
    * available in the previous RDD in the chain.
    */
-  override def compute(split: Partition, context: TaskContext): Iterator[(L, O)] = {
+  override def compute(split: Partition, context: TaskContext): Iterator[(L, R)] = {
     val session = connector.openSession()
-    implicit val pv = protocolVersion(session)
-    val stmt = session.prepare(singleKeyCqlQuery).setConsistencyLevel(consistencyLevel)
-    val bsb = new BoundStatementBuilder[L](rowWriter, stmt, pv, where.values)
+    val bsb = boundStatementBuilder(session)
     val metricsUpdater = InputMetricsUpdater(context, readConf)
     val rowIterator = fetchIterator(session, bsb, left.iterator(split, context))
     val countingIterator = new CountingIterator(rowIterator, limit)
@@ -132,7 +159,8 @@ private[rdd] trait AbstractCassandraJoin[L, O]{
       logDebug(
         f"Fetched ${countingIterator.count} rows " +
           f"from $keyspaceName.$tableName " +
-          f"for partition ${split.index} in $duration%.3f s.")
+          f"for partition ${split.index} in $duration%.3f s."
+      )
       session.close()
     }
     countingIterator
@@ -146,8 +174,8 @@ private[rdd] trait AbstractCassandraJoin[L, O]{
 
   override def getPreferredLocations(split: Partition): Seq[String] = left.preferredLocations(split)
 
-  override def toEmptyCassandraRDD: EmptyCassandraRDD[(L, O)] =
-    new EmptyCassandraRDD[(L, O)](
+  override def toEmptyCassandraRDD: EmptyCassandraRDD[(L, R)] =
+    new EmptyCassandraRDD[(L, R)](
       sc = left.sparkContext,
       keyspaceName = keyspaceName,
       tableName = tableName,
@@ -155,13 +183,8 @@ private[rdd] trait AbstractCassandraJoin[L, O]{
       where = where,
       limit = limit,
       clusteringOrder = clusteringOrder,
-      readConf = readConf)
-
-  private[rdd] def fetchIterator(
-    session: Session,
-    bsb: BoundStatementBuilder[L],
-    lastIt: Iterator[L]): Iterator[(L, O)] 
+      readConf = readConf
+    )
 
 }
-
 
